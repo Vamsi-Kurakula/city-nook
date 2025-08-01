4a97b5d8a6422e3358667007bf59e63837a24345
Vamsi Kurakula <55557753+Vamsi-Kurakula@users.noreply.github.com>
2025-07-31 21:55:30 -0500

    update version for android and apple

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "City Crawler",
     slug: "city-crawler",
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -19,7 +19,7 @@ export default {
     ios: {
       bundleIdentifier: "com.vamsikurakula.citycrawler",
       supportsTablet: true,
-      buildNumber: "1",
+      buildNumber: "2",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "This app uses location to show your position on the crawl map.",
         NSAppTransportSecurity: {
@@ -39,7 +39,7 @@ export default {
     },
     android: {
       package: "com.vamsikurakula.citycrawler",
-      versionCode: 5,
+      versionCode: 6,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
